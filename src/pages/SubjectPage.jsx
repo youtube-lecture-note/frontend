3c774db522abfc57e85dce4946bef8b3871629a0
noir1458 <noir1458@gmail.com>
2025-05-26 23:30:37 +0900

    feat:상위 주제 이동기능, 주제에 저장된 영상 타이틀 보이도록 수정

--- src/pages/SubjectPage.jsx
+++ src/pages/SubjectPage.jsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+
 import TopBar from "../components/TopBar/TopBar";
 import SubjectVideoIcon from "../components/Subject/SubjectVideoIcon";
 
@@ -6,15 +7,18 @@ import SubjectVideoIcon from "../components/Subject/SubjectVideoIcon";
 export default function SubjectPage({ videos }) {
   const { subjectId } = useParams();
   // 유저 카테고리 조회의 videos 객체 받아오기
+  const location = useLocation();
+  const Subjectinfo = location.state?.Subjectinfo;
+  const SubjectVideos = Subjectinfo.videos || [];
 
   return (
     <div className="flex flex-col h-screen">
       <TopBar />
-      <h1 className="text-2xl font-bold">{subjectId}</h1>
-      <div className="flex flex-wrap gap-4">
-        {/* {videos.map((subject) => (
-          <SubjectVideoIcon key={subject} name={subject} />
-        ))} */}
+      <h1 className="text-2xl font-bold">{Subjectinfo.name}</h1>
+      <div className="flex flex-wrap gap-1">
+        {SubjectVideos.map((video) => (
+          <SubjectVideoIcon key={video.videoId} name={video.userVideoName} />
+        ))}
       </div>
     </div>
   );
