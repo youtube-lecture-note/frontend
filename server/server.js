ddb4f26bbdf96cb0c2fcf9e52c185543a72996d2
noir-01 <smu08054@gmail.com>
2025-06-11 21:26:35 +0900

    Feat: 자막 fetch 서버 프록시 추가

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { YoutubeTranscript } = require('youtube-transcript');
+const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const app = express();
 const port = 4000;
@@ -8,11 +9,16 @@ const port = 4000;
 app.use(cors({
   origin: 'http://localhost:3000'  // React 개발 서버
 }));
+const proxyAgent = new HttpsProxyAgent(process.env.PROXY_URL);
 
 app.get('/api/transcript/:videoId', async (req, res) => {
   try {
     const { videoId } = req.params;
-    const transcript = await YoutubeTranscript.fetchTranscript(`https://www.youtube.com/watch?v=${videoId}`);
+    const transcript = await YoutubeTranscript.fetchTranscript(`https://www.youtube.com/watch?v=${videoId}`,
+      {fetchOptions: {
+        agent: proxyAgent
+      }}
+    );
     res.json(transcript);
   } catch (error) {
     res.status(500).json({ error: error.message });
